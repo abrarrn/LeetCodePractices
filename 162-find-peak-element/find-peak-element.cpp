1a329eacce9eb5d3a4ae9c098d32a274271aec07
Abrar Rafid Noor <142677434+abrarrn@users.noreply.github.com>
2023-08-25 06:24:51 +0600

    Time: 0 ms (100.00%) | Memory: 8.9 MB (56.76%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -8,9 +8,7 @@ public:
         int l = 1, r = n-2, m;
         while(l <= r){
             m = (l+r)>>1;
-            if( (m == 0 && nums[m] > nums[m+1]) || 
-                (m == n-1 && nums[m-1] > nums[m-2]) || 
-                (nums[m-1] < nums[m] && nums[m] > nums[m+1])){
+            if(nums[m-1] < nums[m] && nums[m] > nums[m+1]){
                 return m;
             }
             else if(nums[m-1] < nums[m] && nums[m] < nums[m+1]){
