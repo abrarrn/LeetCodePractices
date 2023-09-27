0a3ad97f94ff33fc1499c25a6cace9a7ebc4f41b
Abrar Rafid Noor <142677434+abrarrn@users.noreply.github.com>
2023-09-27 09:23:39 +0600

    Time: 4 ms (23.14%) | Memory: 8 MB (21.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -10,7 +10,19 @@ private:
     }
 public:
     int rob(vector<int>& nums) {
+        //memoization
+        //vector<int> dp(100, -1);
+        //return recurr(nums, nums.size()-1, dp);
+
+        //tabulation
+        if(nums.size()==1)  return nums[0];
+        if(nums.size()==2)  return max(nums[0], nums[1]);
         vector<int> dp(100, -1);
-        return recurr(nums, nums.size()-1, dp);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); ++i){
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.size()-1];
     }
 };
