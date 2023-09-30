264700d85786c143e659f30f12c57d018021ac9e
Abrar Rafid Noor <142677434+abrarrn@users.noreply.github.com>
2023-09-30 09:35:02 +0600

    Time: 0 ms (100.00%) | Memory: 6.4 MB (63.04%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -14,7 +14,7 @@ public:
         //vector<vector<int>> dp(m, vector<int>(n,-1));
         //return recurr(m-1, n-1, dp);
 
-        int dp[m][n];
+        /*int dp[m][n];
         memset(dp, 0, sizeof(dp));
         for(int i=0; i<m; ++i){
             for(int j=0; j<n; ++j){
@@ -22,6 +22,17 @@ public:
                 if(i>0 && j>0)      dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
             }
         }
-        return dp[m-1][n-1];
+        return dp[m-1][n-1];*/
+
+        vector<int> prev(n, 0);
+        vector<int> curr(n, 0);
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                if(i==0 || j==0)    curr[j] = 1;
+                if(i>0 && j>0)      curr[j] = prev[j] + curr[j-1]; 
+            }
+            prev = curr;
+        }
+        return curr[n-1];
     }
 };
