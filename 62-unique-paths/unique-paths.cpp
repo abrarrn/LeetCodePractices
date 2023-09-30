adee9914f491178f78f97ffe554836e825d20ebd
Abrar Rafid Noor <142677434+abrarrn@users.noreply.github.com>
2023-09-30 09:25:51 +0600

    Time: 0 ms (100.00%) | Memory: 6.3 MB (65.99%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int recurr(int r, int c, vector<vector<int>>&dp){
+        //base case
+        if(r == 0 && c == 0)    return 1;
+        if(r < 0 || c < 0)      return 0;
+        if(dp[r][c] != -1)      return dp[r][c];
+
+        int up = recurr(r-1, c, dp);
+        int left = recurr(r, c-1, dp);
+        return dp[r][c] = up + left;
+    }
+    int uniquePaths(int m, int n) {
+        //vector<vector<int>> dp(m, vector<int>(n,-1));
+        //return recurr(m-1, n-1, dp);
+
+        int dp[m][n];
+        memset(dp, 0, sizeof(dp));
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                if(i==0 || j==0)    dp[i][j] = 1;
+                if(i>0 && j>0)      dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
