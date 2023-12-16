db3a132cf705b1794bd69002071e8d68e6e94b16
Abrar Rafid Noor <142677434+abrarrn@users.noreply.github.com>
2023-12-16 21:44:29 +0600

    Time: 3 ms (97.78%), Space: 8 MB (9.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())    
+            return false;
+        int counts[26];
+        memset(counts, 0, sizeof(counts));
+        for(int i=0; i<s.length(); ++i){
+            counts[s[i]-'a']++;
+            counts[t[i]-'a']--;
+        }
+        for(int i=0; i<26; ++i){
+            if(counts[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
